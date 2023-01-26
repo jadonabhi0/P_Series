b78753edbd68f499f7e2e71b9cf7194125a2d69d
Abhishek <jadonabhi0@gmail.com>
2023-01-26 20:04:36 +0530

    .

--- src/Rahul/Qps.java
+++ src/Rahul/Qps.java
@@ -2,13 +2,16 @@ package Rahul;/*
     @author jadon
 */
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class Qps {
 
     public static void main(String[] args) {
 
-
+        System.out.println((int)'a');
 
     }
 
