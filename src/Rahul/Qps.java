c70286e7eb19e36d3ae917c0fd25d414d1746ceb
Abhishek <jadonabhi0@gmail.com>
2023-02-03 18:50:08 +0530

    .

--- src/Rahul/Qps.java
+++ src/Rahul/Qps.java
@@ -9,13 +9,18 @@ import java.util.Stack;
 
 public class Qps {
 
-    public static void main(String[] args) {
+    public static int reverse(int x){
+        int ans = 0;
+        while(x != 0){
+            int rem = x % 10;
+            x /= 10;
+            ans = ans * 10 + rem;
+        }
+        return ans;
+    }
 
-
-
-
-        System.out.println(6 % 7);
-
+    public static void main(String[] args){
+        System.out.println(reverse(-456));
     }
 
 }
