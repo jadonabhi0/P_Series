4fe0374691c6139a3d61e2fbd6e5372c5f1c84c4
Abhishek <jadonabhi0@gmail.com>
2023-02-04 22:19:59 +0530

    .

--- src/Rahul/Qps.java
+++ src/Rahul/Qps.java
@@ -2,25 +2,14 @@ package Rahul;/*
     @author jadon
 */
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.Stack;
+
 
 public class Qps {
 
-    public static int reverse(int x){
-        int ans = 0;
-        while(x != 0){
-            int rem = x % 10;
-            x /= 10;
-            ans = ans * 10 + rem;
-        }
-        return ans;
-    }
+
 
     public static void main(String[] args){
-        System.out.println(reverse(-456));
+
     }
 
 }
